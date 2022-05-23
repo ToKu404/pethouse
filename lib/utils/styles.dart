ac47a27840ca4ec78870820c6f4aedbbf58adbac
Fildzah Azmatun Hasanah <84794687+fildzahah@users.noreply.github.com>
2022-05-23 23:31:23 +0800

    Add style theme (#2)
    * create flutter project
    
    * add style theme
    
    * add style theme
    
    Co-authored-by: toku404 <phantom26isn@gmail.com>

---
+++ lib/utils/styles.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color primaryColor = Color(0xFFECA540);
+const Color secondaryColor = Color(0xFFFF8500);
+
+final TextTheme myTextTheme = TextTheme(
+  headline1: GoogleFonts.poppins(
+      fontSize: 41, fontWeight: FontWeight.w700),
+  headline2: GoogleFonts.poppins(
+      fontSize: 26, fontWeight: FontWeight.w600),
+  headline3: GoogleFonts.poppins(
+      fontSize: 24, fontWeight: FontWeight.w600),
+  headline4: GoogleFonts.poppins(
+      fontSize: 21, fontWeight: FontWeight.w700),
+  headline5: GoogleFonts.poppins(
+      fontSize: 18, fontWeight: FontWeight.w700),
+  headline6: GoogleFonts.poppins(
+      fontSize: 16, fontWeight: FontWeight.w700),
+  subtitle1: GoogleFonts.poppins(
+      fontSize: 12, fontWeight: FontWeight.w600),
+  subtitle2: GoogleFonts.poppins(
+      fontSize: 12, fontWeight: FontWeight.w500),
+  bodyText1: GoogleFonts.poppins(
+      fontSize: 12, fontWeight: FontWeight.w400),
+  bodyText2: GoogleFonts.poppins(
+      fontSize: 12, fontWeight: FontWeight.w300),
+  button: GoogleFonts.poppins(
+      fontSize: 16, fontWeight: FontWeight.w700),
+  caption: GoogleFonts.poppins(
+      fontSize: 10, fontWeight: FontWeight.w400),
+  overline: GoogleFonts.poppins(
+      fontSize: 10, fontWeight: FontWeight.w400),
+);
