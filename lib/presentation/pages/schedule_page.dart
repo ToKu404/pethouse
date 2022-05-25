ef291f7a9bb86134f8d1b490d8ae90fda2eb4058
toku404 <phantom26isn@gmail.com>
2022-05-25 11:16:53 +0800

    add card schedule status

--- lib/presentation/pages/schedule_page.dart
+++ lib/presentation/pages/schedule_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pethouse/utils/styles.dart';
+
+import '../widgets/card_schedule_status.dart';
 
 class SchedulePage extends StatelessWidget {
   const SchedulePage({Key? key}) : super(key: key);
@@ -7,6 +11,36 @@ class SchedulePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Column(
+      children: [
+        InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kPadding),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Neko Task",
+                  style: kTextTheme.headline5,
+                ),
+                const Icon(
+                  FontAwesomeIcons.angleRight,
+                  size: 18,
+                  color: kDarkBrown,
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: kPadding),
+          child: CardScheduleStatus(),
+        ),
+      ],
+    );
   }
 }
---
+++ lib/presentation/widgets/card_schedule_status.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:pethouse/utils/styles.dart';
+
+class CardScheduleStatus extends StatelessWidget {
+  const CardScheduleStatus({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(kPadding * 2),
+      decoration: BoxDecoration(
+          color: kWhite,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+                blurRadius: 13,
+                color: const Color(0xFF000000).withOpacity(.07)),
+            BoxShadow(
+                blurRadius: 5, color: const Color(0xFF000000).withOpacity(.05))
+          ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Daily Task',
+                style: kTextTheme.bodyText1?.copyWith(fontSize: 14),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.check_circle,
+                    color: kSecondaryColor,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: '4/6',
+                        style: kTextTheme.headline3,
+                        children: [
+                          const TextSpan(
+                            text: ' Task',
+                            style: const TextStyle(color: kGreyTransparant),
+                          )
+                        ]),
+                  )
+                ],
+              )
+            ],
+          ),
+          CircularPercentIndicator(
+              radius: 60.0,
+              lineWidth: 10.0,
+              circularStrokeCap: CircularStrokeCap.round,
+              percent: 0.62,
+              progressColor: kSecondaryColor,
+              backgroundColor: kGrey,
+              center: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "62%",
+                    style: kTextTheme.headline5,
+                  ),
+                  Text(
+                    "Done",
+                    style: kTextTheme.bodyText1?.copyWith(height: 1),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
