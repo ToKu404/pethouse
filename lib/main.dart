a7ee4b8f7784b8e289793c8a8c129364a2f3811a
イクサン <54715920+ToKu404@users.noreply.github.com>
2022-05-26 01:31:41 +0800

    create ui adopt page, and create route of splash screen (#26)
    * connect login_page and register_page, create bottom nav, create pet dashboard card
    
    * Add ChangePasswordPage, ProfileEditePage, No InternetPage, and add assets vectors people, user, dog sleep
    
    * add appbar home page
    
    * Add Petrivia Card
    
    * Edited boolean password and confirm password
    
    * finish ui dashboard
    
    * add card schedule status
    
    * add pet slider on schedule page
    
    * finish ui schedule page
    
    * Fixing Route Navigation account_page.dart, edited card_account.dart, and add const SizedBox in check_internet_page.dart
    
    * Add SplashScreen Card Beranda Adopt
    
    * Add Detail Petrivia
    
    * Add pet_description_page.dart and route main.dart, and make make and also move file to directory
    
    * Add pet_description_page.dart
    
    * Add Pet Page
    
    * Add App Bar Widget
    
    * Add Open Adopt
    
    * create ui adopt page, and create route of splash screen
    
    Co-authored-by: Hanan <hananababil07@gmail.com>
    Co-authored-by: adidharmawati <adidharmawati@gmail.com>
    Co-authored-by: Fildzah Azmatun Hasanah <fildzahah00@gmail.com>
    Co-authored-by: Adi Dharma Wati <82831725+adidharmawati@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:pethouse/presentation/pages/account/change_password_page.dart';
 import 'package:pethouse/presentation/pages/account/edit_profile_page.dart';
 import 'package:pethouse/presentation/pages/account/account_page.dart';
+import 'package:pethouse/presentation/pages/adopt_page.dart';
 import 'package:pethouse/presentation/pages/home_page.dart';
 import 'package:pethouse/presentation/pages/mypet/pet_description_page.dart';
 import 'package:pethouse/presentation/pages/other/check_internet_page.dart';
 import 'package:pethouse/presentation/pages/auth/login_page.dart';
 import 'package:pethouse/presentation/pages/auth/register_page.dart';
+import 'package:pethouse/presentation/pages/other/splash_screen_page.dart';
 import 'package:pethouse/utils/styles.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -23,16 +24,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Pethouse',
       theme: ThemeData(colorScheme: kColorScheme),
-      initialRoute: LoginPage.ROUTE_NAME,
+      initialRoute: SplashScreen.ROUTE_NAME,
       routes: {
-        RegisterPage.ROUTE_NAME: (context) => RegisterPage(),
-        LoginPage.ROUTE_NAME: (context) => LoginPage(),
-        HomePage.ROUTE_NAME: (context) => HomePage(),
+        SplashScreen.ROUTE_NAME:(context) => SplashScreen(),
+        RegisterPage.ROUTE_NAME: (context) => const RegisterPage(),
+        LoginPage.ROUTE_NAME: (context) => const LoginPage(),
+        HomePage.ROUTE_NAME: (context) => const HomePage(),
         AccountPage.ROUTE_NAME: (context) => AccountPage(),
-        EditProfilePage.ROUTE_NAME: (context) => EditProfilePage(),
+        EditProfilePage.ROUTE_NAME: (context) => const EditProfilePage(),
         ChangePasswordPage.ROUTE_NAME: (context) => ChangePasswordPage(),
-        CheckInternetPage.ROUTE_NAME: (context) => CheckInternetPage(),
-        PetDescriptionPage.ROUTE_NAME:(context) => PetDescriptionPage(),
+        CheckInternetPage.ROUTE_NAME: (context) => const CheckInternetPage(),
+        PetDescriptionPage.ROUTE_NAME: (context) => const PetDescriptionPage(),
+        AdoptPage.ROUTE_NAME: (context) => const AdoptPage(),
       },
     );
   }
