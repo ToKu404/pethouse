4962949a3039d2de75e2474066392b974e924bd3
Adi Dharma Wati <82831725+adidharmawati@users.noreply.github.com>
2022-05-26 17:16:51 +0800

    Add Modal Pet Adoption Success and Fix FontStyle (#32)
    * connect login_page and register_page, create bottom nav, create pet dashboard card
    
    * Add ChangePasswordPage, ProfileEditePage, No InternetPage, and add assets vectors people, user, dog sleep
    
    * add appbar home page
    
    * Add Petrivia Card
    
    * Edited boolean password and confirm password
    
    * finish ui dashboard
    
    * add card schedule status
    
    * add pet slider on schedule page
    
    * finish ui schedule page
    
    * Fixing Route Navigation account_page.dart, edited card_account.dart, and add const SizedBox in check_internet_page.dart
    
    * Add SplashScreen Card Beranda Adopt
    
    * Add Detail Petrivia
    
    * Add pet_description_page.dart and route main.dart, and make make and also move file to directory
    
    * Add pet_description_page.dart
    
    * Add Pet Page
    
    * Add App Bar Widget
    
    * Add Open Adopt
    
    * create ui adopt page, and create route of splash screen
    
    * Add Schedule Task List Widget
    
    * add btnback_decoration.dart, card_detail_pet.dart, card periodic and medical history, and assets image. Edited change_password_page.dart, pet_description_page.dart.
    
    * Add Modal Feature Unavailable
    
    * Add Modal Adoption Success
    
    * create and finish ui schedule page
    
    * Add Modal Pet Adoption Success and Fix FontStyle
    
    Co-authored-by: toku404 <phantom26isn@gmail.com>
    Co-authored-by: Hanan <hananababil07@gmail.com>
    Co-authored-by: イクサン <54715920+ToKu404@users.noreply.github.com>
    Co-authored-by: Fildzah Azmatun Hasanah <fildzahah00@gmail.com>

---
+++ lib/presentation/widgets/modal_adoption_success.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pethouse/presentation/widgets/gredient_button.dart';
+import 'package:pethouse/utils/styles.dart';
+
+class AdoptionSuccess extends StatelessWidget {
+  const AdoptionSuccess({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 309,
+      width: double.infinity,
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        gradient: const LinearGradient(
+          colors: [Color(0XFFFFE5CE), kWhite],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 14,
+            offset: Offset(2, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: kWhite,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  blurRadius: 10,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SvgPicture.asset(
+                'assets/vectors/success_cat.svg',
+                height: 67,
+                width: 56.83,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Thank You',
+            style: GoogleFonts.poppins(
+              color: Color(0XFFAE531E),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Adoption activities while being processed',
+            style: GoogleFonts.poppins(
+              color: kDarkBrown,
+              fontSize: 10,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            '23 Februari 2022 14:26',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 10,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          GradientButton(
+            height: 39,
+            width: 159,
+            onTap: () {},
+            text: 'View Order Status',
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Container(
+            height: 39,
+            width: 159,
+            decoration: BoxDecoration(
+              color: kWhite,
+              borderRadius: kBorderRadius,
+              border: Border.all(
+                color: kDarkBrown,
+                width: 1.0,
+              ),
+            ),
+            child: MaterialButton(
+              onPressed: () {},
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              shape: RoundedRectangleBorder(
+                borderRadius: kBorderRadius,
+              ),
+              child: Text(
+                'Back to Home',
+                style: const TextStyle(color: kDarkBrown),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
