a638897c565c45dd20acdaeac4d452ea2fc4aa0b
Hanan <hananababil07@gmail.com>
2022-05-26 21:37:25 +0800

    Fixing card_periodic_summary.dart, button adopt_banner_card.dart, and some layout in card widgets

--- lib/presentation/widgets/adopt_banner_card.dart
+++ lib/presentation/widgets/adopt_banner_card.dart
@@ -8,61 +8,58 @@ class AdoptBannerCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => Navigator.pushNamed(context, AdoptPage.ROUTE_NAME),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: kPadding),
-        padding: const EdgeInsets.all(kPadding * 2),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: kDarkBrown, width: 1),
-          gradient: const LinearGradient(
-              colors: [kPrimaryColor, kSecondaryColor],
-              begin: Alignment.centerRight,
-              end: Alignment.centerLeft),
-        ),
-        child: Row(
-          children: [
-            SvgPicture.asset(
-              "assets/vectors/adopt_banner_pet.svg",
-              width: 80,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              width: 20,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Let\'s Make',
-                  style: kTextTheme.bodyText1
-                      ?.copyWith(color: kWhite, fontSize: 15),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: kPadding),
+      padding: const EdgeInsets.all(kPadding * 2),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: kDarkBrown, width: 1),
+        gradient: const LinearGradient(
+            colors: [kPrimaryColor, kSecondaryColor],
+            begin: Alignment.centerRight,
+            end: Alignment.centerLeft),
+      ),
+      child: Row(
+        children: [
+          SvgPicture.asset(
+            "assets/vectors/adopt_banner_pet.svg",
+            width: 80,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Let\'s Make',
+                style: kTextTheme.bodyText1
+                    ?.copyWith(color: kWhite, fontSize: 15),
+              ),
+              Text(
+                'New Friend!',
+                style: kTextTheme.headline5?.copyWith(height: 1.2),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: ()=> Navigator.pushNamed(context, AdoptPage.ROUTE_NAME),
+                child: const Text(
+                  "Adopt Now",
                 ),
-                Text(
-                  'New Friend!',
-                  style: kTextTheme.headline5?.copyWith(height: 1.2),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Adopt Now",
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      primary: kWhite,
-                      onPrimary: kDarkBrown,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                )
-              ],
-            ),
-          ],
-        ),
+                style: ElevatedButton.styleFrom(
+                    primary: kWhite,
+                    onPrimary: kDarkBrown,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
