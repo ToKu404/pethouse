1b77485eb57d3053a9bd8db475395a61a83239e5
Boltzmann <81137510+Hanan07-sys@users.noreply.github.com>
2022-05-27 21:45:12 +0800

    Finishing UI add_new_task.dart (#43)
    * connect login_page and register_page, create bottom nav, create pet dashboard card
    
    * Add ChangePasswordPage, ProfileEditePage, No InternetPage, and add assets vectors people, user, dog sleep
    
    * add appbar home page
    
    * Add Petrivia Card
    
    * Edited boolean password and confirm password
    
    * finish ui dashboard
    
    * add card schedule status
    
    * add pet slider on schedule page
    
    * finish ui schedule page
    
    * Fixing Route Navigation account_page.dart, edited card_account.dart, and add const SizedBox in check_internet_page.dart
    
    * Add SplashScreen Card Beranda Adopt
    
    * Add Detail Petrivia
    
    * Add pet_description_page.dart and route main.dart, and make make and also move file to directory
    
    * Add pet_description_page.dart
    
    * Add Pet Page
    
    * Add App Bar Widget
    
    * Add Open Adopt
    
    * create ui adopt page, and create route of splash screen
    
    * Add Schedule Task List Widget
    
    * add btnback_decoration.dart, card_detail_pet.dart, card periodic and medical history, and assets image. Edited change_password_page.dart, pet_description_page.dart.
    
    * Add Modal Feature Unavailable
    
    * Add Modal Adoption Success
    
    * create and finish ui schedule page
    
    * Add Modal Pet Adoption Success and Fix FontStyle
    
    * Add Modal Delete Pet Cofirmation
    
    * Read More Button on Detail Petrivia
    
    * add modal succes add pet, home schedule no task, schedule no pet
    
    * Fixing card_periodic_summary.dart, button adopt_banner_card.dart, and some layout in card widgets
    
    * Add Ask Page
    
    * Add Medical History Page
    
    * add add_new_task.dart, add route AddNewTaskPage and AddMedicalActivity in main.dart
    
    * add modal medical history, edit card medical, add version splash screen
    
    * add button certificat in pet_description_page.dart
    
    * add detail pet adopt and summary activity card
    
    * Finishing UI add_new_task.dart
    
    Co-authored-by: toku404 <phantom26isn@gmail.com>
    Co-authored-by: イクサン <54715920+ToKu404@users.noreply.github.com>
    Co-authored-by: adidharmawati <adidharmawati@gmail.com>
    Co-authored-by: Fildzah Azmatun Hasanah <fildzahah00@gmail.com>
    Co-authored-by: Adi Dharma Wati <82831725+adidharmawati@users.noreply.github.com>

--- lib/presentation/pages/activity/add_new_task.dart
+++ lib/presentation/pages/activity/add_new_task.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pethouse/presentation/pages/activity/add_medical_activity.dart';
+import 'package:pethouse/presentation/widgets/btnback_decoration.dart';
+import 'package:pethouse/presentation/widgets/gredient_button.dart';
+import 'package:pethouse/utils/styles.dart';
 
 class AddNewTaskActivity extends StatelessWidget {
   static const ROUTE_NAME = "add_newTask_activity";
@@ -8,12 +13,250 @@ class AddNewTaskActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add New Task'),
+        title: Text('New Task',style: TextStyle(color: Colors.black)),
+        leading: btnBack_decoration(),
         centerTitle: true,
+        elevation: 5,
+        backgroundColor: kWhite,
       ),
-      body: Center(
-        child: Container(
-          child: Text('Add New Task'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Ikhsan',
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.poppins(
+                              color: kDarkBrown,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Your best animal  Activity Medical ',
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.poppins(
+                              color: const Color(0XFFCCA88A),
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: kDarkBrown,
+                          width: 1.0,
+                        ),
+                        image: const DecorationImage(
+                          image: NetworkImage(
+                            'https://asset.kompas.com/crops/MzG1rdeLzUk4jJ6KSn-6Sd20igg=/0x0:1920x1280/750x500/data/photo/2021/03/15/604edf099bac9.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Activity',
+                      style: GoogleFonts.poppins(
+                        color: kDarkBrown,
+                        fontSize: 12,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: const DropdownActivityHistory(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 19,
+                    ),
+                    Text(
+                      'Date',
+                      style: GoogleFonts.poppins(
+                        color: kDarkBrown,
+                        fontSize: 12,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              fillColor: const Color(0xFF929292),
+                              labelText: '02 February 2023',
+                              border: OutlineInputBorder(
+                                  borderRadius: kBorderRadius),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.calendar_month,
+                            color: Colors.grey,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 19,
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Start Time',
+                              style: GoogleFonts.poppins(
+                                color: kDarkBrown,
+                                fontSize: 12,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: 136,
+                              height: 42,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                border: Border.all(
+                                  color: Colors.grey,
+                                  width: 1.0,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '11.00 Am', style: kTextTheme.subtitle2,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'End Time',
+                              style: GoogleFonts.poppins(
+                                color: kDarkBrown,
+                                fontSize: 12,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: 137,
+                              height: 42,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                border: Border.all(
+                                  color: Colors.grey,
+                                  width: 1.0,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '13.00 Pm', style: kTextTheme.subtitle2,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 19,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        fillColor: const Color(0xFF929292),
+                        labelText: 'Repeat',
+                        border: OutlineInputBorder(borderRadius: kBorderRadius),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 19,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        fillColor: const Color(0xFF929292),
+                        labelText: 'Description',
+                        border: OutlineInputBorder(borderRadius: kBorderRadius),
+                      ),
+                      maxLines: 3,
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                  ),
+                  child: GradientButton(
+                      height: 50,
+                      width: double.infinity,
+                      onTap: () {},
+                      text: 'Schedule'),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
