58825d0b43643ef20ca2609ce5a253b97c6ac659
イクサン <54715920+ToKu404@users.noreply.github.com>
2022-05-27 18:17:55 +0800

    add detail pet adopt and summary activity card (#42)
    * connect login_page and register_page, create bottom nav, create pet dashboard card
    
    * Add ChangePasswordPage, ProfileEditePage, No InternetPage, and add assets vectors people, user, dog sleep
    
    * add appbar home page
    
    * Add Petrivia Card
    
    * Edited boolean password and confirm password
    
    * finish ui dashboard
    
    * add card schedule status
    
    * add pet slider on schedule page
    
    * finish ui schedule page
    
    * Fixing Route Navigation account_page.dart, edited card_account.dart, and add const SizedBox in check_internet_page.dart
    
    * Add SplashScreen Card Beranda Adopt
    
    * Add Detail Petrivia
    
    * Add pet_description_page.dart and route main.dart, and make make and also move file to directory
    
    * Add pet_description_page.dart
    
    * Add Pet Page
    
    * Add App Bar Widget
    
    * Add Open Adopt
    
    * create ui adopt page, and create route of splash screen
    
    * Add Schedule Task List Widget
    
    * add btnback_decoration.dart, card_detail_pet.dart, card periodic and medical history, and assets image. Edited change_password_page.dart, pet_description_page.dart.
    
    * Add Modal Feature Unavailable
    
    * Add Modal Adoption Success
    
    * create and finish ui schedule page
    
    * Add Modal Pet Adoption Success and Fix FontStyle
    
    * Add Modal Delete Pet Cofirmation
    
    * Read More Button on Detail Petrivia
    
    * add modal succes add pet, home schedule no task, schedule no pet
    
    * Fixing card_periodic_summary.dart, button adopt_banner_card.dart, and some layout in card widgets
    
    * Add Ask Page
    
    * Add Medical History Page
    
    * add add_new_task.dart, add route AddNewTaskPage and AddMedicalActivity in main.dart
    
    * add modal medical history, edit card medical, add version splash screen
    
    * add button certificat in pet_description_page.dart
    
    * add detail pet adopt and summary activity card
    
    Co-authored-by: Hanan <hananababil07@gmail.com>
    Co-authored-by: adidharmawati <adidharmawati@gmail.com>
    Co-authored-by: Fildzah Azmatun Hasanah <fildzahah00@gmail.com>
    Co-authored-by: Adi Dharma Wati <82831725+adidharmawati@users.noreply.github.com>

--- lib/presentation/pages/mypet/pet_description_page.dart
+++ lib/presentation/pages/mypet/pet_description_page.dart
@@ -4,11 +4,14 @@ import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:pethouse/presentation/widgets/card_detail_pet.dart';
 import 'package:pethouse/presentation/widgets/card_medical_history.dart';
 import 'package:pethouse/presentation/widgets/card_periodic_summary.dart';
+import 'package:pethouse/presentation/widgets/daily_summary_card.dart';
 import 'package:pethouse/presentation/widgets/modal_medical_history.dart';
 import 'package:pethouse/utils/styles.dart';
 import 'package:colorful_iconify_flutter/icons/twemoji.dart';
 import 'package:colorful_iconify_flutter/icons/noto.dart';
 import 'package:iconify_flutter/icons/carbon.dart';
+
+import '../../widgets/card_account.dart';
 
 class PetDescriptionPage extends StatelessWidget {
   final String description =
@@ -19,237 +22,247 @@ class PetDescriptionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<DailySummary> _dailySummary = [
+      DailySummary('Feeds', '09:00', 'assets/icons/icon_feeds.svg', .9),
+      DailySummary('Walks', '10:00', 'assets/icons/icon_walks.svg', .8),
+      DailySummary('Pee', '11:00', 'assets/icons/icon_pee.svg', .65),
+      DailySummary('Vitamin', '12:00', 'assets/icons/icon_vitamin.svg', .4),
+    ];
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  SizedBox(
-                    height: 362,
-                    child: Image.asset(
-                      'assets/images/image_cat.png',
-                      fit: BoxFit.fill,
-                    ),
+        child: ListView(
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  height: 362,
+                  child: Image.asset(
+                    'assets/images/image_cat.png',
+                    fit: BoxFit.fill,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        InkWell(
-                          onTap: () => Navigator.pop(context),
-                          child: Container(
-                            width: 32,
-                            height: 32,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0x30000000)),
-                            child: Icon(
-                              FontAwesomeIcons.arrowLeft,
-                              size: 24,
-                              color: kWhite,
-                            ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child: Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0x30000000)),
+                          child: Icon(
+                            FontAwesomeIcons.arrowLeft,
+                            size: 24,
+                            color: kWhite,
                           ),
                         ),
-                        Spacer(),
-                        InkWell(
-                          onTap: () => {},
-                          child: Container(
-                            width: 32,
-                            height: 32,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0x30000000)),
-                            child: Icon(
-                              Icons.more_vert,
-                              size: 24,
-                              color: kWhite,
-                            ),
+                      ),
+                      Spacer(),
+                      InkWell(
+                        onTap: () => {},
+                        child: Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0x30000000)),
+                          child: Icon(
+                            Icons.more_vert,
+                            size: 24,
+                            color: kWhite,
                           ),
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 4, left: 16, right: 16, bottom: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              'Ikhsan',
-                              style: kTextTheme.headline4,
-                            ),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.cake,
-                                  color: kOrange,
-                                  size: 18,
-                                ),
-                                Text(
-                                  '17 May 2020',
-                                  style: kTextTheme.overline,
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                        Spacer(),
-                        InkWell(
-                          onTap: () => {},
-                          child: Container(
-                            height: 32,
-                            width: 41,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              color: kOrange,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: kGreyTransparant,
-                                  offset: Offset(0.0, 1.0), //(x,y)
-                                  blurRadius: 6.0,
-                                )
-                              ],
-                            ),
-                            padding: EdgeInsets.all(3),
-                            child: Iconify(
-                              Carbon.certificate,
-                              color: kWhite,
-                            ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 4, left: 16, right: 16, bottom: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'Ikhsan',
+                            style: kTextTheme.headline4,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.cake,
+                                color: kOrange,
+                                size: 18,
+                              ),
+                              Text(
+                                '17 May 2020',
+                                style: kTextTheme.overline,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      InkWell(
+                        onTap: () => {},
+                        child: Container(
+                          height: 32,
+                          width: 41,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: kOrange,
+                            boxShadow: [
+                              BoxShadow(
+                                color: kGreyTransparant,
+                                offset: Offset(0.0, 1.0), //(x,y)
+                                blurRadius: 6.0,
+                              )
+                            ],
+                          ),
+                          padding: EdgeInsets.all(3),
+                          child: Iconify(
+                            Carbon.certificate,
+                            color: kWhite,
                           ),
                         ),
-                      ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CardDetailPet(
+                        type: 'Gender',
+                        content: 'Male',
+                      ),
+                      CardDetailPet(
+                        type: 'Age',
+                        content: '2 Year',
+                      ),
+                      CardDetailPet(
+                        type: 'Breed',
+                        content: 'Scottish',
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    description,
+                    style: kTextTheme.bodyText2,
+                  ),
+                  SizedBox(
+                    height: 19,
+                  ),
+                  CardMedicalHistory(),
+                  SizedBox(
+                    height: 19,
+                  ),
+                  Text(
+                    'Daily Summary',
+                    style: kTextTheme.headline4,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 7 / 8,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
                     ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CardDetailPet(
-                          type: 'Gender',
-                          content: 'Male',
-                        ),
-                        CardDetailPet(
-                          type: 'Age',
-                          content: '2 Year',
-                        ),
-                        CardDetailPet(
-                          type: 'Breed',
-                          content: 'Scottish',
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      description,
-                      style: kTextTheme.bodyText2,
-                    ),
-                    SizedBox(
-                      height: 19,
-                    ),
-                    CardMedicalHistory(),
-                    SizedBox(
-                      height: 19,
-                    ),
-                    Text(
-                      'Daily Summary',
-                      style: kTextTheme.headline4,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CardDetailPet(
-                          type: 'Gender',
-                          content: 'Male',
-                        ),
-                        CardDetailPet(
-                          type: 'Age',
-                          content: '2 Year',
-                        ),
-                        CardDetailPet(
-                          type: 'Breed',
-                          content: 'Scottish',
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 19,
-                    ),
-                    Text(
-                      'Periodic Summary',
-                      style: kTextTheme.headline4,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: kOrange, borderRadius: kBorderRadius),
-                      child: Padding(
-                        padding: const EdgeInsets.all(kPadding),
-                        child: Column(
-                          children: [
-                            CardPeriodicSummary(
-                                iconfy: Iconify(
-                                  Noto.shower,
-                                  size: 36,
-                                ),
-                                titlePeriodic: 'Shower',
-                                typePeriodic: 'Activity',
-                                datePeriodic: '20 Agustus 2022'),
-                            CardPeriodicWeight(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: _dailySummary.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final ds = _dailySummary[index];
+                      return DailySummaryCard(
+                          dailySummary : ds);
+                    },
+                  ),
+                  SizedBox(
+                    height: 19,
+                  ),
+                  Text(
+                    'Periodic Summary',
+                    style: kTextTheme.headline4,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: kOrange, borderRadius: kBorderRadius),
+                    child: Padding(
+                      padding: const EdgeInsets.all(kPadding),
+                      child: Column(
+                        children: [
+                          CardPeriodicSummary(
                               iconfy: Iconify(
-                                Twemoji.man_lifting_weights,
+                                Noto.shower,
                                 size: 36,
+                              ),
+                              titlePeriodic: 'Shower',
+                              typePeriodic: 'Activity',
+                              datePeriodic: '20 Agustus 2022'),
+                          CardPeriodicWeight(
+                            iconfy: Iconify(
+                              Twemoji.man_lifting_weights,
+                              size: 36,
+                            ),
+                            titlePeriodic: 'Weight',
+                            typePeriodic: 'Measured',
+                            datePeriodic: '20 September 2022',
+                            weightValue: 4.3,
+                          ),
+                          CardPeriodicSummary(
+                              iconfy: Iconify(
+                                Twemoji.drop_of_blood,
+                                size: 34,
                               ),
                               titlePeriodic: 'Weight',
                               typePeriodic: 'Measured',
-                              datePeriodic: '20 September 2022',
-                              weightValue: 4.3,
-                            ),
-                            CardPeriodicSummary(
-                                iconfy: Iconify(
-                                  Twemoji.drop_of_blood,
-                                  size: 34,
-                                ),
-                                titlePeriodic: 'Weight',
-                                typePeriodic: 'Measured',
-                                datePeriodic: '18 Januari 2022'),
-                            CardPeriodicSummary(
-                                iconfy: Iconify(
-                                  Noto.toothbrush,
-                                  size: 34,
-                                ),
-                                titlePeriodic: 'Weight',
-                                typePeriodic: 'Measured',
-                                datePeriodic: '3 Februari 2023'),
-                          ],
-                        ),
+                              datePeriodic: '18 Januari 2022'),
+                          CardPeriodicSummary(
+                              iconfy: Iconify(
+                                Noto.toothbrush,
+                                size: 34,
+                              ),
+                              titlePeriodic: 'Weight',
+                              typePeriodic: 'Measured',
+                              datePeriodic: '3 Februari 2023'),
+                        ],
                       ),
-                    )
+                    ),
+                  )
 
-                    // CardDetailPet(type: 'Feeds',content: 'Thanks',),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  // CardDetailPet(type: 'Feeds',content: 'Thanks',),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class DailySummary {
+  final String title;
+  final String time;
+  final String icon;
+  final double progress;
+
+  DailySummary(this.title, this.time, this.icon, this.progress);
 }
--- lib/presentation/widgets/adopt_card.dart
+++ lib/presentation/widgets/adopt_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pethouse/presentation/pages/detail_adopt_page.dart';
 import 'package:pethouse/utils/styles.dart';
 
 class AdoptCard extends StatelessWidget {
@@ -7,107 +8,113 @@ class AdoptCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: kWhite,
-        boxShadow: [
-          BoxShadow(
-              blurRadius: 13, color: const Color(0xFF000000).withOpacity(.07)),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                'https://media.istockphoto.com/photos/ginger-cat-sitting-on-floor-in-living-room-and-looking-at-camera-pet-picture-id1149347768?b=1&k=20&m=1149347768&s=170667a&w=0&h=eYqyMdhTK02QUU0miN0-ka-Dk37hNU8uTpF4z1k-t3A=',
-                height: 135,
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () => Navigator.pushNamed(context, DetailAdoptPage.ROUTE_NAME),
+      child: Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: kWhite,
+          boxShadow: [
+            BoxShadow(
+                blurRadius: 13,
+                color: const Color(0xFF000000).withOpacity(.07)),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  'https://media.istockphoto.com/photos/ginger-cat-sitting-on-floor-in-living-room-and-looking-at-camera-pet-picture-id1149347768?b=1&k=20&m=1149347768&s=170667a&w=0&h=eYqyMdhTK02QUU0miN0-ka-Dk37hNU8uTpF4z1k-t3A=',
+                  height: 135,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Positioned(
-              top: 5,
-              right: 5,
-              child: Container(
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: kWhite),
-                width: 25,
-                height: 25,
-                child: Center(
-                  child: SvgPicture.asset(
-                    'assets/icons/cat_icon_outline.svg',
-                    color: kPrimaryColor,
-                    width: 15,
-                    height: 15,
-                    fit: BoxFit.cover,
+              Positioned(
+                top: 5,
+                right: 5,
+                child: Container(
+                  decoration:
+                      BoxDecoration(shape: BoxShape.circle, color: kWhite),
+                  width: 25,
+                  height: 25,
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/icons/cat_icon_outline.svg',
+                      color: kPrimaryColor,
+                      width: 15,
+                      height: 15,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ]),
-          Container(
-            width: 140,
-            margin: EdgeInsets.only(top: 5),
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Pesu',
-                  style: kTextTheme.subtitle1?.copyWith(color: kDarkBrown),
-                ),
-                Text(
-                  'Ras: Persia',
-                  style: kTextTheme.overline
-                      ?.copyWith(color: kGreyTransparant, height: 1),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Color(0xFFCDDDF6),
-                      ),
-                      child: Text(
-                        'Jantan',
-                        style: kTextTheme.overline?.copyWith(
-                          color: Color(0xFF578EE0),
+            ]),
+            Container(
+              width: 140,
+              margin: EdgeInsets.only(top: 5),
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Pesu',
+                    style: kTextTheme.subtitle1?.copyWith(color: kDarkBrown),
+                  ),
+                  Text(
+                    'Ras: Persia',
+                    style: kTextTheme.overline
+                        ?.copyWith(color: kGreyTransparant, height: 1),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xFFCDDDF6),
+                        ),
+                        child: Text(
+                          'Jantan',
+                          style: kTextTheme.overline?.copyWith(
+                            color: Color(0xFF578EE0),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Color(0xFFFFF5E3),
+                      SizedBox(
+                        width: 4,
                       ),
-                      child: Text(
-                        '10 Bulan',
-                        style: kTextTheme.overline?.copyWith(
-                          color: Color(0xFF85634D),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xFFFFF5E3),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              ],
+                        child: Text(
+                          '10 Bulan',
+                          style: kTextTheme.overline?.copyWith(
+                            color: Color(0xFF85634D),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          )
-        ],
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ),
       ),
     );
   }
