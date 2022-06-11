d02bb3f85f0af256dca4ada06989bfdc9d88a396
toku404 <phantom26isn@gmail.com>
2022-06-11 15:06:00 +0800

    update splash ui

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -2,4 +2,5 @@ library core;
 
 export 'package:core/utils/constants.dart';
 export 'package:core/utils/observer.dart';
+export 'package:core/utils/text_consts.dart';
 export 'package:core/utils/routes.dart';
---
+++ core/lib/utils/text_consts.dart
@@ -0,0 +1,4 @@
+// Splash Const Text
+const kAppTitle = 'Pethouse';
+const kAppDesc = 'Join and Discover The Best Habit Tracker for Your Pet';
+const kAppVersion = 'Version 1.0';
