c3c9fe6d5f92d9d07266c3e3f7358aaee4cca65e
Hanan <hananababil07@gmail.com>
2022-06-17 18:16:57 +0800

    finish content about_page.dart

--- user/lib/presentation/pages/profile_pages/about_page.dart
+++ user/lib/presentation/pages/profile_pages/about_page.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:core/core.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:user/data/models/about.dart';
 
 class AboutPage extends StatelessWidget {
@@ -11,87 +13,152 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AboutPage'),
+        title: const Text('AboutPage'),
       ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 120,
-                  width: 120,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: kPrimaryColor),
-                  child: Padding(
-                      padding: EdgeInsets.all(8),
-                      child: SvgPicture.asset(dataAppModel.logo)),
+              children: <Widget>[
+                Row(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: kPrimaryColor),
+                      child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: SvgPicture.asset(dataAppModel.logo)),
+                    ),
+                    const SizedBox(width: 20),
+                    Text(dataAppModel.title,
+                        style: kTextTheme.headline5
+                            ?.copyWith(color: kPrimaryColor, fontSize: 32)),
+                  ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                Text(dataAppModel.title,
-                    style: kTextTheme.headline5?.copyWith(color: kPrimaryColor)),
-                SizedBox(
-                  height: 20,
+                Text(
+                  'Deskripsi',
+                  style: kTextTheme.bodyText1?.copyWith(
+                      fontWeight: FontWeight.bold, color: kDarkBrown),
                 ),
-                Text('Deskripsi',style: kTextTheme.bodyText1?.copyWith(fontWeight: FontWeight.bold,color: kDarkBrown),),
-                SizedBox(
+                const SizedBox(
                   height: 5,
                 ),
-                Text(dataAppModel.description,textAlign: TextAlign.justify,style: kTextTheme.bodyText2),
-                SizedBox(
+                Text(dataAppModel.description,
+                    textAlign: TextAlign.justify, style: kTextTheme.bodyText2),
+                const SizedBox(
                   height: 20,
                 ),
-                Text('Bahasa Pemrograman',style: kTextTheme.bodyText1?.copyWith(fontWeight: FontWeight.bold,color: kDarkBrown),),
-                SizedBox(
+                Text(
+                  'Bahasa Pemrograman',
+                  style: kTextTheme.bodyText1?.copyWith(
+                      fontWeight: FontWeight.bold, color: kDarkBrown),
+                ),
+                const SizedBox(
                   height: 5,
                 ),
-                Text(dataAppModel.programming_leng,style: kTextTheme.bodyText2),
-                SizedBox(
+                Text(dataAppModel.programming_leng,
+                    style: kTextTheme.bodyText2),
+                const SizedBox(
                   height: 20,
                 ),
-                Text('Framework',style: kTextTheme.bodyText1?.copyWith(fontWeight: FontWeight.bold,color: kDarkBrown),),
-                SizedBox(
+                Text(
+                  'Framework',
+                  style: kTextTheme.bodyText1?.copyWith(
+                      fontWeight: FontWeight.bold, color: kDarkBrown),
+                ),
+                const SizedBox(
                   height: 5,
                 ),
-                Text(dataAppModel.framework,style: kTextTheme.bodyText2),
-                SizedBox(
+                Text(dataAppModel.framework, style: kTextTheme.bodyText2),
+                const SizedBox(
                   height: 20,
                 ),
-                Text('Database',style: kTextTheme.bodyText1?.copyWith(fontWeight: FontWeight.bold,color: kDarkBrown),),
-                SizedBox(
+                Text(
+                  'Database',
+                  style: kTextTheme.bodyText1?.copyWith(
+                      fontWeight: FontWeight.bold, color: kDarkBrown),
+                ),
+                const SizedBox(
                   height: 5,
                 ),
-                Text(dataAppModel.database,style: kTextTheme.bodyText2),
-                SizedBox(
+                Text(dataAppModel.database, style: kTextTheme.bodyText2),
+                const SizedBox(
                   height: 20,
                 ),
-                Text('Package/Depedencies',style: kTextTheme.bodyText1?.copyWith(fontWeight: FontWeight.bold,color: kDarkBrown),),
-                SizedBox(
+                Text(
+                  'Package/Depedencies',
+                  style: kTextTheme.bodyText1?.copyWith(
+                      fontWeight: FontWeight.bold, color: kDarkBrown),
+                ),
+                const SizedBox(
                   height: 5,
                 ),
-                // ListView.separated(
-                //   itemCount: dataPackagesList.length,
-                //   itemBuilder: (context, index) {
-                //     final DataPackages dataPackages = dataPackagesList[index];
-                //     return ListTile(
-                //       title: Text(dataPackages.name),
-                //       subtitle: Text(dataPackages.link),
-                //       trailing: Text(dataPackages.version),
-                //     );
-                //   },
-                //   separatorBuilder: (context,index) => const Divider(color: kGreyTransparant,),
-                // ),
+                ListView.builder(
+                    shrinkWrap: true,
+                    primary: false,
+                    itemCount: dataPackagesList.length,
+                    itemBuilder: (context, index) {
+                      final DataPackages dataPackages = dataPackagesList[index];
+                      return ListTile(
+                        title: Text(dataPackages.name),
+                        subtitle: HyperlinkText(dataPackages.link),
+                        dense: true,
+                        trailing: Text(dataPackages.version),
+                      );
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Assets',
+                  style: kTextTheme.bodyText1?.copyWith(
+                      fontWeight: FontWeight.bold, color: kDarkBrown),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                ListView.builder(
+                    shrinkWrap: true,
+                    primary: false,
+                    itemCount: dataAssetsList.length,
+                    itemBuilder: (context, index) {
+                      final DataAssets dataAssets = dataAssetsList[index];
+                      return ListTile(
+                        title: Text(dataAssets.name),
+                        subtitle: HyperlinkText(dataAssets.link),                        dense: true,
+                      );
+                    }),
               ],
             ),
           ),
-
         ),
       ),
     );
+  }
+}
+
+class HyperlinkText extends StatelessWidget {
+  final String text;
+  HyperlinkText(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+        text: TextSpan(
+            text: text,
+            style: TextStyle(
+                decoration: TextDecoration.underline, color: Colors.blue),
+                recognizer: TapGestureRecognizer()..onTap = (){
+                launch(text);
+                }
+        ));
   }
 }
