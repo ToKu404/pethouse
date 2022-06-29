152d6d306fe1cc7ad812c5e9fe755f540eec2d9d
イクサン <54715920+ToKu404@users.noreply.github.com>
2022-06-29 15:54:48 +0800

    Fix Bug and Finishing (#81)
    * Revert "search empty solve"
    
    This reverts commit c4b5aec80734c295ca2579f99d1438b74ae786d9.
    
    * Revert "search empty solve"
    
    This reverts commit f80a5c4ff6df99e0aaf94fecaeede9890419ea63.
    
    * solve bug and finishing

--- core/lib/presentation/blocs/internet_check/internet_check_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'internet_check_cubit.dart';
-
-abstract class InternetCheckState {}
-
-class InternetCheckInitial extends InternetCheckState {}
-
-class InternetCheckLost extends InternetCheckState {}
-
-class InternetCheckGain extends InternetCheckState {}
